cf4579faebab81751d1e634a64ba0f59f33d700e
Priya <priyamaheshwari183@gmail.com>
2024-08-07 03:35:28 +0530

    tailwind and props

--- vitereact01/src/App.jsx
+++ vitereact01/src/App.jsx
@@ -1,8 +1,13 @@
 import Chai from './chai'
 function App() {
+  const username = "priya maheshwari"
 
   return (
+    <>
     <Chai />
+    <h1>Hello {username}</h1>
+    <p>test para</p>
+    </>
   )
 }
 
--- vitereact01/src/main.jsx
+++ vitereact01/src/main.jsx
@@ -2,8 +2,39 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+function MyApp(){
+  return(
+    <div>
+      <h1>Custom App !</h1>
+    </div>
+  )
+}
+
+// const ReactElement = {
+//   type: 'a',
+//   props: {
+//       href: 'https://google.com',
+//       target: '_blank'
+//   },
+//   children: 'Click me to visit Google'
+// }
+
+const anotherElement = (
+  <a href='https://google.com' target='_blank'>Visit Google</a>
+)
+
+const anotherUser = "React"
+
+const reactElement = React.createElement(
+  'a',
+  {href: 'https://google.com', target: '_blank'},
+  'Click here',
+  anotherElement
+)
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+
+    reactElement
+    // <App />
+
 )
